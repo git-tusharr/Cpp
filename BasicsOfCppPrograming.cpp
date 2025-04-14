84c096fd0dc2030c4ec6584368990ae81c2a87f3
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-04-14 15:10:16 +0530

    first commit

---
+++ BasicsOfCppPrograming.cpp
@@ -0,0 +1,186 @@
+// // 3rd class of c++ cybrom(basic maths calculations)
+
+# include <iostream>
+using namespace std;
+int main()
+{
+   
+
+
+//Mathematical operation two numbers
+
+// int a;
+// int b;
+// cout<<"enter the value of a ";
+// cin >> a;
+// cout<<"enter the value of b ";
+// cin >> b;
+// cout<<a+b<<endl;
+// cout<<a-b<<endl;
+// cout<<a*b<<endl;
+// cout<<a/b<<endl;
+// cout<<a%b<<endl;
+
+// // Area of Square :-
+
+// int side;
+// cout <<"enter the value of side ";
+// cin >>side;
+// cout <<"area of square = "<<side*side;
+
+
+// Area of Circle :-
+
+// int radius;
+// cout <<"enter the vaule of radius ";
+// cin >>radius;
+// cout <<"Area of circle = "<<(radius*radius)*3.14;
+
+
+// // 4th Class of C++(Percentage calculator ,profit calculator,simple interst)
+
+
+
+// // ****4th class of c++ cybrom****\\ \\
+
+// Percentage Calculator
+
+// int Hindi;
+// int English;
+// int Cpp;
+// int TotalMarks = 300;
+// int TotalObtainedMarks;
+
+// cout <<"Enter the marks of Hindi ";
+// cin>>Hindi;
+// cout<<"Enter the marks of English ";
+// cin>>English;
+// cout<<"Enter the marks of Cpp ";
+// cin>>Cpp;
+// TotalObtainedMarks=English+Hindi+Cpp;
+// cout<<"Total Percentage = "<<((TotalObtainedMarks)*100)/TotalMarks<<"%";
+
+// Profit Or Loss Calculator
+
+// int CP;
+// int SP;
+// int MP;
+// int Profit;
+// int Discount;
+// cout<<"enter CP,SP,MP,Profit,Discount";
+// cin>>CP>>SP>>MP>>Profit>>Discount;
+// cout <<"Profit="<<(SP-CP);
+
+
+// Simple Interst
+// int Principal,Rate,Time;
+// cout<<"Enter Principal ";
+// cin>>Principal;
+// cout<<"Enter Rate ";
+// cin>>Rate;
+// cout<<"Enter Time ";
+// cin>>Time;
+// cout<<"Simple Interst = "<<(Principal*Rate*Time)/100;
+
+
+
+ // 5th class of c++ cybrom \\
+
+// Avg weight of 10 students was 50 kgs it becomes 45 kgs when one student left the group find the weight of student that left .
+
+// int NoOfStudents=10;
+// int AvgWt=50;
+// int NewNoOfStudents=9;
+// int NewAvgWt=45;
+// int result=NoOfStudents*AvgWt-NewNoOfStudents*NewAvgWt;
+// cout<<"Weight of the student that left the group = "<<result;
+
+
+
+
+// Arithmetic progression
+
+// 1 3 5 7 9 11 13
+// Nth Term = a+(n-1)d
+// 5th term = 1+4*2
+
+// int a=1;
+// int n=5;
+// int d=2;
+// int answer=a+(n-1)*d;
+// cout<<answer;
+
+// GP series 
+
+// Formula a*r^(n-1)
+// a=first term
+// r=common ratio
+
+// 1 3 9 27 81
+// int a=1;
+// int r=3
+// int n=4
+// int ans;
+// ans=a*pow(r,n-1)  // using cmath library
+// cout<<ans;
+
+
+
+// Fibonaacci Series;
+
+// int ft=1;
+// int st=2;
+// int next=ft+st;
+// ft=st;
+// st=next;
+
+//nth term series
+//1 11 111 1111 11111
+
+//Swaping(exchange)
+// int a=10;
+// int b=11;
+// int c;
+// a=11;
+// b=10;
+
+// Override
+
+// c=a;
+// a=b;
+// b=c;
+
+// // swaping with three variables
+// c=a;
+// a=b;
+// b=c;
+// cout<<a<<b;
+
+// // with the help of two variables
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// cout<<a<<b;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int NoOfStudents=10;
+int AvgWt=50;
+int NewNoOfStudents=9;
+int NewAvgWt=45;
+int result=NoOfStudents*AvgWt-NewNoOfStudents*NewAvgWt;
+cout<<"Weight of the student that left the group = "<<result;
