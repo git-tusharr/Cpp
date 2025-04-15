8a480d5e1f6e46fe844a76f6954b8201df5212fb
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-04-15 13:01:58 +0530

    Data Types and Operators

---
+++ DataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<limits>
+using namespace std;
+int main()
+
+{
+// // Data Types 
+
+
+// // Size of a data type
+
+// cout<<"Size of double in bytes"<<sizeof(double);
+
+// // Address of a Variable
+
+// int a;
+// cout<<"Address = "<<&a;
+
+// // Limits of an datatype{include <limits> header file}    
+  
+// cout<<static_cast<int>(numeric_limits<char>::min());
+
+}
---
+++ Operators.cpp
@@ -0,0 +1,29 @@
+# include<iostream>
+using namespace std;
+int main()
+
+{
+// // OPERATORS = To perform operations on operands
+
+// // Binary Operators :-  Work with one operand
+
+// Arithmetic (+,-,*,/,%)
+// Relational(>,<,=,>=,<=,!=)
+// Assignment(+=,-=,==,*=,/=,%=)
+// Logical(and or not exor)
+// Bitwise Operator(and or not exor)
+
+// // Unary operators :-  Work with two operand
+
+// Increment (++)(Increment by one)
+    // Pre Increment ++a
+    // Post Increment a++
+// Decrement (--)(Decrement by one)
+     // Pre Decrement --a
+     // Post Decrement a--
+
+
+     int a=10;
+     cout<<++a;
+     cout<<a;
+    }
